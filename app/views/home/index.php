4b860130bbafa1fa06a68a454a6cc1ff9af3e615
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-06-25 18:37:49 +0700

    config: helpers

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -2,6 +2,7 @@
     <div class="jumbotron mt-4">
       <h1 class="display-4">Welcome</h1>
       <hr class="my-4">
+      <h1><?= Utils::Greeting(); ?></h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet blanditiis repudiandae, odit sed eaque quos ullam, explicabo repellat labore ad unde molestias saepe. Architecto, quaerat labore ipsam facere asperiores qui!</p>
       <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
     </div>
--- config/config.php
+++ config/config.php
@@ -1,5 +1,6 @@
-<?php 
+<?php
 
+define('ROOT_PATH', dirname(__DIR__));
 define('BASEURL', 'http://localhost/personal/php-starter/web');
 
 // DB
@@ -7,3 +8,11 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', 'root');
 define('DB_NAME', 'phpmvc');
+
+
+spl_autoload_register(function ($class_name) {
+    $file = ROOT_PATH . '/app/helpers/' . $class_name . '.php';
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
